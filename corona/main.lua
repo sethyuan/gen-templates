3b4a3e05627e519efa26e31bfafe23bcbb1afb19
sethyuan <cavingdeep@gmail.com>
2013-07-25 09:40:30 +0800

    Added Corona templates.

---
+++ corona/main.lua
@@ -0,0 +1,5 @@
+-- Set a fits it all background image.
+local bg = display.newImageRect("bg.png", 360, 570)
+bg:setReferencePoint(display.CenterReferencePoint)
+bg.x = display.contentCenterX
+bg.y = display.contentCenterY
