91100bfd38c25c272e8f580ce8718d2862756ab6
sethyuan <cavingdeep@gmail.com>
2014-04-20 20:47:02 +0800

    Fix app entry point.

--- clojure/___/bin/src/__name_for_file__/core.clj
+++ clojure/___/bin/src/__name_for_file__/core.clj
@@ -1,6 +1,8 @@
-(ns {{{name}}}.core)
+(ns {{{name}}}.core
+  (:require [clojure.core.match :refer [match]]
+            [cav.macros :refer :all]
+            [cav.core :as cav])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
+(defn -main [& args]
   (println x "Hello, World!"))
