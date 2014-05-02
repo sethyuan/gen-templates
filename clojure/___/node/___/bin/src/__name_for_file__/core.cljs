6edd3d52687a9d1df8c5715773274513db24dae4
sethyuan <cavingdeep@gmail.com>
2014-05-02 10:12:27 +0800

    Add println support.

--- clojure/___/node/___/bin/src/__name_for_file__/core.cljs
+++ clojure/___/node/___/bin/src/__name_for_file__/core.cljs
@@ -1,7 +1,9 @@
 (ns {{{name}}}.core
+  (:require [cljs.nodejs :as nodejs])
   #_(:require-macros [{{{name}}}.macros]))
 
 (defn -main [& args]
   (.log js/console "Hello ClojureScript"))
 
+(nodejs/enable-util-print!)
 (set! *main-cli-fn* -main)
