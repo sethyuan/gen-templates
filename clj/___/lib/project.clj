260882a36a4377e763c8f046d3c6533e39c0e97c
sethyuan <cavingdeep@gmail.com>
2014-10-21 09:38:02 +0800

    Updated Clojure lib template.

--- clj/___/lib/project.clj
+++ clj/___/lib/project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* true}
-  :profiles {:aot {:aot :all}}
+  :profiles {:release {:aot :all}}
   :jvm-opts ^:replace []
   :plugins [[codox "0.8.10"]]
   :codox {:language :clojure
