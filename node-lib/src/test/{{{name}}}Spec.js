4c7395bd67ab32d073db88d6291e09557e9566d2
sethyuan <sethyuan@gmail.com>
2015-05-17 23:38:43 +0800

    Add test support for node-lib.

---
+++ node-lib/src/test/{{{name}}}Spec.js
@@ -0,0 +1,9 @@
+require("source-map-support").install();
+
+import * as {{{name}}} from "../lib/{{{name}}}"
+
+describe("suite", () => {
+  it("test case", () => {
+    expect(true).toBe(true);
+  });
+});
