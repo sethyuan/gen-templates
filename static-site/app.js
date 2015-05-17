339c3dd018b36e7e112f3074ec25c834b2a20522
sethyuan <sethyuan@gmail.com>
2015-05-17 23:36:59 +0800

    Remove some outdated templates.

--- static-site/app.js
+++ static-site/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
+var compression = require("compression");
 var path = require("path");
 var app = express();
 var port = {{{port}}};
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(port, function() {
